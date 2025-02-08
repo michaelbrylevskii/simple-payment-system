f04b0527ddfaa59f28b084324e433d84527e1987
Michael Brylevskii <michael.brylevskii@gmail.com>
2025-02-07 23:28:54 -0300

    [accounts] Created from template

---
+++ accounts/src/main/kotlin/HTTP.kt
@@ -0,0 +1,40 @@
+package com.michaelbrylevskii.sps
+
+import com.codahale.metrics.*
+import io.github.flaxoos.ktor.server.plugins.kafka.Kafka
+import io.github.flaxoos.ktor.server.plugins.kafka.MessageTimestampType
+import io.github.flaxoos.ktor.server.plugins.kafka.TopicName
+import io.github.flaxoos.ktor.server.plugins.kafka.admin
+import io.github.flaxoos.ktor.server.plugins.kafka.common
+import io.github.flaxoos.ktor.server.plugins.kafka.consumer
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerConfig
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerRecordHandler
+import io.github.flaxoos.ktor.server.plugins.kafka.producer
+import io.github.flaxoos.ktor.server.plugins.kafka.registerSchemas
+import io.github.flaxoos.ktor.server.plugins.kafka.topic
+import io.ktor.client.HttpClient
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.metrics.dropwizard.*
+import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.micrometer.prometheus.*
+import java.sql.Connection
+import java.sql.DriverManager
+import java.util.concurrent.TimeUnit
+import org.jetbrains.exposed.sql.*
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
+
+fun Application.configureHTTP() {
+    routing {
+        swaggerUI(path = "openapi")
+    }
+}
---
+++ accounts/src/main/kotlin/HelloService.kt
@@ -0,0 +1,5 @@
+package com.michaelbrylevskii.sps
+
+fun interface HelloService {
+    fun sayHello()
+}
